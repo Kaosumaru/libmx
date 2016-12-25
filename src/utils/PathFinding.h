1314f9851ab066cfa68398e00de6a37c9a474032
Mateusz Borycki <mateuszborycki@gmail.com>
2016-12-25 18:21:07 +0100

    Allow range of start points in PathFInding.

--- src/utils/PathFinding.h
+++ src/utils/PathFinding.h
@@ -18,27 +18,44 @@ namespace MX
 
 		using SetDistanceIfLowerFunc = std::function<bool(const NodeType& node, DistanceType distance)>;
 
+		using QueuePair = std::pair<DistanceType, NodeType>;
+		using PriorityQueue = std::priority_queue<QueuePair, std::vector<QueuePair>, std::greater<QueuePair>>;
+
 		struct Functions
 		{
 			GetNeighborsFunc getNeighbors;
 			SetDistanceIfLowerFunc setDistanceIfLower;
 		};
 
+		DijkstraPathFinding(const std::vector<NodeType>& startNodes, const Functions& f)
+		{
+			PriorityQueue queue;
+
+			DistanceType startDistance = 0.0f;
+			for (auto& start : startNodes)
+			{
+				queue.push(std::make_pair(startDistance, start));
+				f.setDistanceIfLower(start, startDistance);
+			}
+
+			PathFind(queue, f);
+		}
+
 		DijkstraPathFinding(const NodeType& start, const Functions& f)
 		{
-			_f = f;
+			PriorityQueue queue;
 
-
-
-			using Pair = std::pair<DistanceType, NodeType>;
-			std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair>> queue;
-
-			
 			DistanceType startDistance = 0.0f;
 			queue.push(std::make_pair(startDistance, start));
 			f.setDistanceIfLower(start, startDistance);
 
+			PathFind(queue, f);
+		}
 
+
+	protected:
+		void PathFind(PriorityQueue& queue, const Functions& f)
+		{
 			while (!queue.empty())
 			{
 				auto p = queue.top();
@@ -57,14 +74,7 @@ namespace MX
 				};
 				f.getNeighbors(p.second, forN);
 			}
-
 		}
-
-
-	protected:
-		Functions _f;
-
-
 	};
 
 	template<typename NodeType>
@@ -79,7 +89,29 @@ namespace MX
 
 		DefaultDijkstraPathFinding(const NodeType& start, const GetNeighborsFunc& getNeighbors)
 		{
-			auto setDistanceIfLower = [&](const NodeType& node, DistanceType distance) -> bool
+			auto setDistanceIfLower = CreateCallback();
+			typename DijkstraPath::Functions f;
+			f.getNeighbors = getNeighbors;
+			f.setDistanceIfLower = setDistanceIfLower;
+
+			DijkstraPath Dijkstra(start, f);
+		}
+
+		DefaultDijkstraPathFinding(const std::vector<NodeType>& start, const GetNeighborsFunc& getNeighbors)
+		{
+			auto setDistanceIfLower = CreateCallback();
+			typename DijkstraPath::Functions f;
+			f.getNeighbors = getNeighbors;
+			f.setDistanceIfLower = setDistanceIfLower;
+
+			DijkstraPath Dijkstra(start, f);
+		}
+
+		const auto& distances() { return _distances; }
+	protected:
+		auto CreateCallback()
+		{
+			return [&](const NodeType& node, DistanceType distance) -> bool
 			{
 				auto r = _distances.insert(std::make_pair(node, distance));
 				if (r.second)
@@ -91,17 +123,8 @@ namespace MX
 				old_pair.second = distance;
 				return true;
 			};
-
-
-			typename DijkstraPath::Functions f;
-			f.getNeighbors = getNeighbors;
-			f.setDistanceIfLower = setDistanceIfLower;
-
-			DijkstraPath Dijkstra(start, f);
 		}
 
-		const auto& distances() { return _distances; }
-	protected:
 		MapType _distances;
 	};
 
