cbb56baa977bb11cd6c15e2bd5a44964e69bfc84
Mateusz Borycki <mateuszborycki@gmail.com>
2019-12-03 23:24:13 +0100

    Added shader tokenizer & added include macro to it.

--- src/graphic/opengl/Program.cpp
+++ src/graphic/opengl/Program.cpp
@@ -1,6 +1,12 @@
 #include "Program.h"
 #include "Shader.h"
 #include "game/resources/Resources.h"
+#include "slang/ast.h"
+#include "slang/lexer.h"
+#include "slang/parser.h"
+#include "slang/preprocessor.h"
+#include "slang/print.h"
+#include <sstream>
 
 using namespace MX;
 using namespace MX::gl;
@@ -9,10 +15,66 @@ namespace MX
 {
 namespace gl
 {
+    const std::vector<slang::Token>& tokenizeShader(const std::string& shaderPath)
+    {
+        using namespace slang;
+        static std::map<std::string, std::vector<Token>> shaders;
+        auto& tokens = shaders[shaderPath];
+
+        if (tokens.empty())
+        {
+            auto shaderBody = MX::Resources::get().openTextFile(shaderPath);
+            Keywords keys;
+            keys.add_all_keywords();
+
+            std::istringstream is { shaderBody };
+            Logger logger(shaderPath, true);
+            Lexer lexer(is, keys, logger);
+            Preprocessor pp(lexer, logger, [](int ver, Profile p) {
+                std::cout << "GLSL version: " << ver << " " << p << std::endl;
+                return true;
+            });
+            pp.register_builtin_macros();
+
+            pp.register_macro("include", Macro({ { "path", 0 } }, [&lexer](const std::vector<Macro::Arg>& args) {
+                auto& pathArg = args[0];
+                std::ostringstream p;
+                for (auto& t : pathArg)
+                    p << t;
+                auto path = p.str();
+
+                return tokenizeShader(path);
+            }));
+
+            Token tok;
+            while (true)
+            {
+                tok = pp.preprocess();
+                if (tok.type() == Token::END)
+                    break;
+                tokens.push_back(tok);
+            }
+        }
+
+        return tokens;
+    }
+
+    std::string preprocessShader(const std::string& shaderPath)
+    {
+        std::ostringstream ss;
+        auto& tokens = tokenizeShader(shaderPath);
+
+        for (auto& t : tokens)
+            ss << t << " ";
+
+        return ss.str();
+    }
+
     Program::pointer createProgramFromFiles(const std::string& vertexShader, const std::string& fragmentShader, std::string& errorLog)
     {
-        std::string vertex = MX::Resources::get().openTextFile(vertexShader);
-        std::string fragment = MX::Resources::get().openTextFile(fragmentShader);
+        auto vertex = preprocessShader(vertexShader);
+        auto fragment = preprocessShader(fragmentShader);
+
         return createProgram(vertex, fragment, errorLog);
     }
 
